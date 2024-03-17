398359c598d7a1bf3250a18cc7dc456e60549304
ideos2040kyui <kido.y.ad@m.titech.ac.jp>
2024-03-17 12:20:08 +0900

    fix

--- tag_extract.py
+++ tag_extract.py
@@ -1,3 +1,14 @@
+"""
+extract_tag_chatGPT ：chatGPTを用いてタグ生成を行う関数
+引数：("テキストかurlの文字列", [既存のタグのリスト])
+戻り値：[生成したタグのリスト]
+"""
+"""
+extract_tag ：Mecabを用いてタグ生成を行う関数
+引数：("テキストかurlの文字列")
+戻り値：[生成したタグのリスト]
+"""
+
 import os
 import sys
 import re
@@ -52,9 +63,9 @@ def extract_tag(text_or_url):
     input_text = extract_url(text_or_url)
   else:  
     input_text = text_or_url
-  if len(input_text) > 500: input_text = input_text[:500]
-  reccomend_tag_list = [pair[0] for pair in ma_parse(input_text)[:100]][:10]
-  print(reccomend_tag_list)
+  if len(input_text) > 5000: input_text = input_text[:5000]
+  reccomend_tag_list = [pair[0] for pair in ma_parse(input_text)][:10]
+  # print(reccomend_tag_list)
   return reccomend_tag_list
 
 def extract_tag_chatgpt(text_or_url, tag_list):
@@ -85,13 +96,16 @@ def extract_tag_chatgpt(text_or_url, tag_list):
   conversation.append({"role": "assistant", "content": answer})
   answer = re.findall(r'\d*\. .*?\n', answer)
   reccomend_tag_list = [re.sub(r'\d*\. (.*?)', '\\1', i).replace("\n", "") for i in answer]
-  print(reccomend_tag_list)
+  # print(reccomend_tag_list)
   return reccomend_tag_list
 
 if __name__ == '__main__':
 
-  extract_tag_chatgpt("https://note.nkmk.me/python-re-match-search-findall-etc/", ["物理学", "数学", "正規表現"])
-
   sentence = "ここに述べる理論は、今日一般に「相対性理論」と呼ばれている理論*3の、考えうる限り一般化したものである。以下ではこの新しい理論を「相対性理論」と呼び以前の「特殊相対性理論」とは区別するとともに、後者の知識を前提とする。相対性理論の一般化は、ミンコフスキーにより与えられた特殊相対論の形式によって、大いに容易となった。かの数学者は空間座標と時間座標の等価性を初めて見出し、私の理論に使いやすい形にしてくれた。一般相対論に必要な数学的道具は、ガウス、リーマン、クリストッフェルによる非ユークリッド的*4な多様体*5の研究に基づき、リッチ、レヴィ・チヴィタにより統合され、既に理論物理学の問題に利用されている、絶対微分幾何学の中で既に完成されている。この論文の理解のために数学書を勉強する必要が無いよう、私はチャプターB*6の中で、我々に必要だが、物理学者にとって当然の前提とはされない数学的道具を全て、できる限り簡単にそして見通しの良い形で構築した。最後にこの場を借りて私の友人であり数学者のグロスマンに感謝したい。彼のおかげで関連する数学書の勉強を省くことができたし、彼は私が重力場の方程式*7を見つけるのも助けてくれた。"
+  url = "https://note.nkmk.me/python-re-match-search-findall-etc/"
 
-  extract_tag("https://note.nkmk.me/python-re-match-search-findall-etc/")
+  extract_tag_chatgpt(sentence, ["物理学", "数学", "正規表現"])
+  extract_tag_chatgpt(url, ["物理学", "数学", "正規表現"])
+
+  extract_tag(sentence)
+  extract_tag(url)
